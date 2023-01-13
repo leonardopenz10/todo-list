54022064bc2b10ee570b81978bfd6575945ef121
Leonardo <leonardopenz.ti@gmail.com>
2023-01-12 22:40:38 -0300

    Criado o botão Add e funcionalidade de adicionar nova tarefa

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,14 @@
-let newTask = $("#taskLine").val
+let newTask = 0
 
-console.log(newTask)
+$("#addNewTask").click(() => {
+
+    if($("#taskLine").val() != 0){
+        newTask = $("#taskLine").val()
+        $("li").html('<li class="taskListLine">'+newTask+'</li>')
+        console.log(newTask)
+    }else
+    
+    console.log("campo não preenchido")
+
+} )
+
