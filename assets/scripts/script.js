1c973869d25c01e25e843228d739941af13cdc72
Leonardo <leonardopenz.ti@gmail.com>
2023-02-17 20:49:13 -0300

    Adicionado icons nas tarefas e novas tarefas

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -4,7 +4,7 @@ $("#addNewTask").click(() => {
 
     if($("#taskLine").val() != 0){
         newTask = $("#taskLine").val()
-        $("#taskList").prepend('<li class="taskListLine">'+newTask+'</li>')
+        $("#taskList").prepend('<li class="taskListLine"> <img src="./assets/images/no-check.png">'+newTask+'<img src="./assets/images/cancel-task.png"> </li>')
         console.log(newTask)
 
         $("#taskLine").val("")
@@ -13,4 +13,8 @@ $("#addNewTask").click(() => {
     alert("Campo não preenchido! Gentileza preencha o campo de nova tarefa.")
 
 } )
+
+$(".taskListLine").click(() => {
+    $("li").toogle("taskListFinished")
+})
 
